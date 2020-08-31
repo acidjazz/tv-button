547983decfbcfd483a798df6957a0e21e70ad09d
Kevin Olson <acidjazz@gmail.com>
2020-08-31 03:16:22 -0500

    ✅ unit testing fixes

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   presets: [
-    '@vue/ap',
+    '@vue/app',
   ],
 };
--- test/tv-button.spec.js
+++ test/tv-button.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import TvButton from '@/tv-button'
+import PushButton from '@/PushButton'
 
 describe('tv-button', () => {
     test('is a Vue instance', () => {
-        const wrapper = mount(TvButton)
+        const wrapper = mount(PushButton)
         expect(wrapper.vm).toBeTruthy()
     })
 })
