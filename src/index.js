0050504c935b3c2155cef4f87435675504b3a3eb
Kevin Olson <acidjazz@gmail.com>
2020-09-07 02:11:53 -0500

    ✨ proper plugin installation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import PushButton from './PushButton.vue'
 import ButtonGroup from './ButtonGroup.vue'
-const components = [ PushButton, ButtonGroup ]
+const components = {
+  PushButton: PushButton,
+  ButtonGroup: ButtonGroup
+}
 
 export default {
   install (Vue, options = {}) {
-    components.forEach(component => {
-      Vue.component(component.name, component)
-    })
+    for (var key in components) {
+      Vue.component(key, components[key])
+    }
   }
 }
+
 export { PushButton, ButtonGroup }
