022d3d98517c370f5482a7bdbc2d4e6dc14ee9d8
kevin olson <acidjazz@gmail.com>
2021-05-23 03:44:21 -0500

    ✨ trying another simpler method for typing

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,12 @@
-export * from './tvbutton.components'
+import Vue from 'vue'
+import { PluginFunction } from 'vue'
+import { PushButton } from './PushButton'
+
+export function install (vue: typeof Vue): void
+
+declare class TvButton {
+  static install: PluginFunction<never>
+  PushButton: PushButton
+}
+
+export default TvButton
--- types/tvbutton.components.d.ts
+++
@@ -1,7 +0,0 @@
-import { PushButton } from './PushButton'
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    PushButton: typeof PushButton
-  }
-}
