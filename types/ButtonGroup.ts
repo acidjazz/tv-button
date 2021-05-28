cdae5b8ef853ac8b37d9033a7ceaf29d44c171ca
kevin olson <acidjazz@gmail.com>
2021-05-28 18:42:18 -0500

    ✨ add buttongroup to the types

---
+++ types/ButtonGroup.ts
@@ -0,0 +1,3 @@
+import Vue from 'vue'
+export declare class ButtonGroup extends Vue {
+}
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import { PluginFunction } from 'vue'
 import { PushButton } from './PushButton'
+import { ButtonGroup } from './ButtonGroup'
 
 export function install (vue: typeof Vue): void
 
 declare class TvButton {
   static install: PluginFunction<never>
   PushButton: PushButton
+  ButtonGroup: ButtonGroup
 }
 
 export default TvButton
